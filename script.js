25e9d9b5b1822bb1f056dbe32f735f2efc90aec4
FreedyOmar <132398985+FreedyOmar@users.noreply.github.com>
2026-02-05 21:47:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+const carrusel = document.getElementById("carrusel");
+const items = document.querySelectorAll(".item");
+const tarjetas = document.querySelectorAll(".tarjeta");
+
+const total = items.length;
+const angulo = 360 / total;
+const radio = 280;
+
+/* Posicionar círculo */
+items.forEach((item, i) => {
+    item.style.transform =
+        `rotateY(${angulo*i}deg) translateZ(${radio}px)`;
+});
+
+/* Giro carrusel */
+let rotacion = 0;
+let arrastrando = false;
+let inicioX = 0;
+
+carrusel.addEventListener("mousedown", e=>{
+    arrastrando = true;
+    inicioX = e.clientX;
+});
+
+window.addEventListener("mouseup", ()=>{
+    arrastrando = false;
+});
+
+window.addEventListener("mousemove", e=>{
+    if(!arrastrando) return;
+
+    let mover = e.clientX - inicioX;
+    inicioX = e.clientX;
+
+    rotacion += mover * 0.3;
+
+    carrusel.style.transform =
+        `rotateY(${rotacion}deg)`;
+});
+
+/* Touch */
+carrusel.addEventListener("touchstart", e=>{
+    arrastrando = true;
+    inicioX = e.touches[0].clientX;
+});
+
+window.addEventListener("touchend", ()=>{
+    arrastrando = false;
+});
+
+window.addEventListener("touchmove", e=>{
+    if(!arrastrando) return;
+
+    let mover = e.touches[0].clientX - inicioX;
+    inicioX = e.touches[0].clientX;
+
+    rotacion += mover * 0.4;
+
+    carrusel.style.transform =
+        `rotateY(${rotacion}deg)`;
+});
+
+/* Flip individual */
+tarjetas.forEach(t=>{
+    t.addEventListener("click", e=>{
+        e.stopPropagation();
+        t.classList.toggle("voltear");
+    });
+});
+
+/* 🌸 PETALOS REALISTAS DINAMICOS */
+const contenedorPetalos = document.querySelector(".petalos");
+
+for(let i=0;i<35;i++){
+
+    let petalo = document.createElement("span");
+
+    petalo.style.left = Math.random()*100 + "vw";
+    petalo.style.animationDuration = (8 + Math.random()*8) + "s";
+    petalo.style.animationDelay = Math.random()*5 + "s";
+
+    // Tamaño variable
+    let size = 0.7 + Math.random()*0.8;
+    petalo.style.transform = "scale(" + size + ")";
+
+    contenedorPetalos.appendChild(petalo);
+}
